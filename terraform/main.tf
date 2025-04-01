6f1333c0d7106a244e9f78a8521f24f11e85bb2e
Pratiklanjewar2025 <pratiklanjewar22@gmail.com>
2025-04-01 15:59:15 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,13 +2,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "main_server" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Ubuntu AMI, update as needed
+resource "aws_instance" "new_server" {
+  ami           = "ami-12345678"
   instance_type = "t2.micro"
+  user_data     = file("userdata.sh")
+}
 
-  user_data = file("terraform/userdata.sh")
-
-  tags = {
-    Name = "MainServer"
-  }
+output "new_server_ip" {
+  value = aws_instance.new_server.public_ip
 }
